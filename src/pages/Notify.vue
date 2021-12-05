d4b1de6ac8b249ebedfcbb58ce02bbf5fae19628
Yaros <Yaroslavhik@yandex.ua>
2021-12-05 11:55:11 +0100

    initial commit

---
+++ src/pages/Notify.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+
+    <section>
+      <div class="container">
+        <h1>Notify Page</h1>
+      </div>
+
+    </section>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Notify"
+}
+</script>
+
+<style scoped>
+
+</style>
