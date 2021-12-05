89250e30fd4afa939c1504daa12fc5994853c11d
Yaros <Yaroslavhik@yandex.ua>
2021-12-05 12:24:41 +0100

    added styles into NotifyPage and deleted table.scss

--- src/pages/NotifyPage.vue
+++ src/pages/NotifyPage.vue
@@ -3,11 +3,25 @@
 
     <section>
       <div class="container">
-        <Notify
-            :messages="messages"
-        ></Notify>
-      </div>
 
+        <!--        wrapper-->
+        <div class="notify__wrapper">
+
+          <!--          title-->
+          <div class="notify-title">
+            <p>Notify App</p>
+            <!--            svg-->
+          </div>
+
+          <!--          notify-->
+          <div class="notify__content">
+            <Notify
+                :messages="messages"
+            ></Notify>
+          </div>
+
+        </div>
+      </div>
     </section>
 
   </div>
@@ -35,6 +49,32 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 90vh;
+}
 
+.notify__wrapper {
+  width: 400px;
+  background-color:#ffffff;
+  padding: 30px;
+  border-radius: 16px;
+  box-shadow: 0 12px 22px 0 rgba(0,0,0,1);
+}
+
+.notify__content {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  min-height: 300px;
+}
+
+.notify-title {
+  p {
+    font-size: 24px;
+  }
+}
 </style>
