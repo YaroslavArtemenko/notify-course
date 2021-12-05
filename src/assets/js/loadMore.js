7797b8da8eb5b28a74e5bf0674fec7befcb74656
Yaros <Yaroslavhik@yandex.ua>
2021-12-05 13:55:34 +0100

    added script(logic) to loadMore.js

---
+++ src/assets/js/loadMore.js
@@ -0,0 +1,23 @@
+export default function loadMore (res) {
+    const messages = []
+
+    // if > 2
+    if (res.length > 2) {
+        for (let i = 0; i < 2; i++) {
+            // main - false to main - true
+            res[i].main = true
+            messages.push(res[i])
+        }
+        // back messages
+        return messages
+    }
+    // if < 2
+    else {
+        for (let i = 0; i < res.length; i++) {
+            res[i].main = true
+            messages.push(res[i])
+        }
+        // back messages
+        return messages
+    }
+}
